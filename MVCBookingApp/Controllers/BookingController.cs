7a3a81e51428a1e5393cd5a239395b262960f5a8
Marko Šeparović <maseparov@vsite.hr>
2024-01-18 18:09:24 +0100

    Add project files.

---
+++ MVCBookingApp/Controllers/BookingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBookingApp.Models;
+
+
+namespace MVCBookingApp.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> hotelBookings = new List<HotelBooking>();
+        public IActionResult Index()
+        {
+            return View(hotelBookings);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBooking(HotelBooking hotelBooking)
+        {
+            hotelBookings.Add(hotelBooking);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCBookingApp/Models/HotelBooking.cs
@@ -0,0 +1,17 @@
+﻿namespace MVCBookingApp.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public required string GuestName { get; set; }
+        public int RoomNumber { get; set; }
+
+
+        private static int nextId = 1;
+        public HotelBooking()
+        {
+            Id = nextId;
+            nextId++;
+        }
+    }
+}
